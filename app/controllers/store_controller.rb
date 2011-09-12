530389d185c5c696d8d98483459bbb864ed04e45
Lukasz Harezlak <lukasz.harezlak@gmail.com>
2011-09-12 16:32:54 +0200

    Cart and associated features added

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
   def index
 		@products = Product.all
+		@viewed = increment_counter
   end
+
+	def increment_counter
+		session[:index_viewed] ||= 0
+		session[:index_viewed] += 1
+	end
 
 end
